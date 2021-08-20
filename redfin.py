5a55292bc00fed42cf7ac8d92776413038809ea0
Ashley King <kingan1@appstate.edu>
2021-08-20 10:19:44 -0400

    enabled linting

--- redfin.py
+++ redfin.py
@@ -35,20 +35,20 @@ time.sleep(10)
 files = os.listdir()
 """
 # get the most recent file, which is the downloaded file
-filename = max([ f for f in os.listdir()],key=os.path.getctime)
+filename = max([f for f in os.listdir()], key=os.path.getctime)
 print(filename)
 # move the file into data
-os.rename(filename, f'data/{filename}')
+os.rename(filename, f"data/{filename}")
 filename = "data/" + filename
-print('done')
-csvfile = open(filename, 'r')
-jsonfile = open('data/file.json', 'w')
-#fieldnames = ('SALE TYPE','SOLD DATE','PROPERTY TYPE','ADDRESS,CITY,STATE OR PROVINCE','ZIP OR POSTAL CODE','PRICE','BEDS','BATHS','LOCATION','SQUARE FEET','LOT SIZE','YEAR BUILT','DAYS ON MARKET','$/SQUARE FEET','HOA/MONTH','STATUS','NEXT OPEN HOUSE START TIME','NEXT OPEN HOUSE END TIME','URL (SEE http://www.redfin.com/buy-a-home/comparative-market-analysis FOR INFO ON PRICING)','SOURCE','MLS#','FAVORITE','INTERESTED','LATITUDE','LONGITUDE')
-reader = csv.DictReader( csvfile)
+print("done")
+csvfile = open(filename, "r")
+jsonfile = open("data/file.json", "w")
+# fieldnames = ('SALE TYPE','SOLD DATE','PROPERTY TYPE','ADDRESS,CITY,STATE OR PROVINCE','ZIP OR POSTAL CODE','PRICE','BEDS','BATHS','LOCATION','SQUARE FEET','LOT SIZE','YEAR BUILT','DAYS ON MARKET','$/SQUARE FEET','HOA/MONTH','STATUS','NEXT OPEN HOUSE START TIME','NEXT OPEN HOUSE END TIME','URL (SEE http://www.redfin.com/buy-a-home/comparative-market-analysis FOR INFO ON PRICING)','SOURCE','MLS#','FAVORITE','INTERESTED','LATITUDE','LONGITUDE')
+reader = csv.DictReader(csvfile)
 lst = []
 for row in reader:
     lst.append(row)
     json.dump(row, jsonfile)
-    jsonfile.write('\n')
+    jsonfile.write("\n")
 json.dump(lst, jsonfile)
 jsonfile.close()
