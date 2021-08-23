e33efd4b16e3057172ba4b05c8a149da74d4ecc6
Ashley King <kingan1@appstate.edu>
2021-08-22 21:15:14 -0400

    Removed json references and changed to only houses

--- redfin.py
+++ redfin.py
@@ -1,10 +1,15 @@
-from selenium import webdriver
-import time
 import os.path
+import time
+
+from selenium import webdriver
 
 # Initializing firefox browser
 # Have to configure how files are saved
+from selenium.webdriver.firefox import options
+
 firefoxProfile = webdriver.FirefoxProfile()
+opt = options.Options()
+opt.headless = True
 direc = os.getcwd()
 firefoxProfile.set_preference("browser.download.panel.shown", False)
 firefoxProfile.set_preference(
@@ -15,7 +20,7 @@ firefoxProfile.set_preference(
 )
 firefoxProfile.set_preference("browser.download.folderList", 2)
 firefoxProfile.set_preference("browser.download.dir", direc)
-driver = webdriver.Firefox(firefoxProfile)
+driver = webdriver.Firefox(firefox_profile=firefoxProfile, options=opt)
 
 
 # Open up the Raleigh page
@@ -24,12 +29,14 @@ driver.get("http://www.redfin.com/city/35711/NC/Raleigh/filter/property-type=hou
 time.sleep(5)
 # Click on the "Remove Outline" button to expand our view
 driver.find_element_by_class_name("RemoveOutlineButton").click()
-# # zoom out 2 times to query more homes
+time.sleep(3)
+
+# zoom out 2 times to query more homes
 zoomOut = driver.find_element_by_class_name("zoomMinusControlButton")
-zoomOut.click()
-time.sleep(2)
-zoomOut.click()
-time.sleep(2)
+
+for _ in range(3):
+    zoomOut.click()
+    time.sleep(3)
 
 # download the current selection
 button = driver.find_element_by_id("download-and-save")
@@ -45,14 +52,14 @@ button.click()
 time.sleep(2)
 driver.quit()
 
-###################
+# ##################
 # File downloaded #
-###################
+# ##################
 
-filename = max([f for f in os.listdir()], key=os.path.getctime)
-print(filename)
-# move the file into data
-os.rename(filename, f"data/{filename}")
-filename = "data/" + filename
-# also update the file.csv file to be this file
-os.replace(filename, "data/file.csv")
+# filename = max([f for f in os.listdir()], key=os.path.getctime)
+# print(filename)
+# # move the file into data
+# os.rename(filename, f"data/{filename}")
+# filename = "data/" + filename
+# # also update the file.csv file to be this file
+# os.replace(filename, "data/file.csv")
